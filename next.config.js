dbb176c9fe32bf655445b81e5e9457c11add0f93
Shoaib Shaikh <shaikhsho292@gmail.com>
2023-01-08 18:03:33 +0530

    Delete next.config.js

--- next.config.js
+++
@@ -1,9 +0,0 @@
-/** @type {import('next').NextConfig} */
-const nextConfig = {
-  reactStrictMode: true,
-  env: {
-    NEWS_API_KEY: "8c11b3eaeac84eeeb3a091908bcd36ac",
-  },
-};
-
-module.exports = nextConfig;
